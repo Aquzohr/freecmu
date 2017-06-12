9fe821344f9111127ec238f32fe925fa9ec1d43f
aquzohr <kampee.expertly01@gmail.com>
2017-06-12 15:04:50 +0700

    add ssl fb-login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider  :facebook, 
             '233569517104162', '363d21877aeb38cd6671315ab110c944',
-            scope: 'public_profile', display: 'page', image_size: 'square'
+            scope: 'public_profile', display: 'page', image_size: 'square',
+            :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
