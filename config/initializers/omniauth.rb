14875b12d175eeb6109d83fd45626aeef4ed4a2b
aquzohr <kampee.expertly01@gmail.com>
2017-06-12 15:22:12 +0700

    fix ssl heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   provider  :facebook, 
             '233569517104162', '363d21877aeb38cd6671315ab110c944',
             scope: 'public_profile', display: 'page', image_size: 'square',
