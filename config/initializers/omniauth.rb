455c1f59ba3256cda3e80d21bb5c19d0f14aaa9c
aquzohr <kampee.expertly01@gmail.com>
2017-06-13 10:40:10 +0700

    fix ssl fb v3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider  :facebook, 
             '233569517104162', '363d21877aeb38cd6671315ab110c944',
             scope: 'public_profile', display: 'page', image_size: 'square',
-            :client_options => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}
+            secure_image_url: 'true'
 end
