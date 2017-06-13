7b83f59a8456f920bc914bda0eb98c1099bac578
aquzohr <kampee.expertly01@gmail.com>
2017-06-13 10:10:08 +0700

    ssl fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   provider  :facebook, 
             '233569517104162', '363d21877aeb38cd6671315ab110c944',
             scope: 'public_profile', display: 'page', image_size: 'square',
-            :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
+            { :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}} }
 end
